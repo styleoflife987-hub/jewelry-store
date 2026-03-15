5d8e9644a7415c5d6c93a5a78ef17139b429a28c
styleoflife987-hub <styleoflife987@gmail.com>
2026-03-15 20:56:20 +0530

    Update payment.js

--- js/payment.js
+++ js/payment.js
@@ -1,27 +1,23 @@
-function payNow(amount){
-
-var options = {
-
-key: "RAZORPAY_KEY",
-
-amount: amount * 100,
-
-currency: "INR",
-
-name: "Style Of Life Jewelry",
-
-description: "Jewelry Purchase",
-
-handler: function (response){
-
-alert("Payment Successful");
-
-}
-
-};
-
-var rzp1 = new Razorpay(options);
-
-rzp1.open();
-
+function payNow(amount) {
+    var options = {
+        key: CONFIG.RAZORPAY_KEY,  // Use from config
+        amount: amount * 100,
+        currency: "INR",
+        name: "Style Of Life Jewelry",
+        description: "Jewelry Purchase",
+        handler: function(response) {
+            alert("Payment Successful. Payment ID: " + response.razorpay_payment_id);
+            // Clear cart after successful payment
+            localStorage.removeItem("cart");
+            window.location.href = "track.html?order=" + response.razorpay_payment_id;
+        },
+        prefill: {
+            name: "Customer Name",
+            email: "customer@example.com",
+            contact: "9999999999"
+        }
+    };
+    
+    var rzp1 = new Razorpay(options);
+    rzp1.open();
 }
