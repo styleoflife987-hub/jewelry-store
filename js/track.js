8ad0b7311f23a2e509f31f299d750ce54240d8fc
styleoflife987-hub <styleoflife987@gmail.com>
2026-03-15 14:24:39 +0530

    Create track.js

---
+++ js/track.js
@@ -0,0 +1,18 @@
+const API="https://script.google.com/macros/s/AKfycby3wzak-aaQ66x5UKbC2_htO6H-qt9dp0eEsyUblO2_5X5t5b1Nd0FtZY4HCiaV6QBf_g/exec";
+
+function track(){
+
+let id=document.getElementById("orderId").value;
+
+fetch(API+"?action=track&id="+id)
+
+.then(r=>r.json())
+
+.then(data=>{
+
+document.getElementById("result").innerText=
+JSON.stringify(data,null,2);
+
+});
+
+}
