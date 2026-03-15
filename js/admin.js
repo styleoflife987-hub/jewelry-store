5b8ede630763ccedfd58cf04ecf3ad411736923b
styleoflife987-hub <styleoflife987@gmail.com>
2026-03-15 14:26:04 +0530

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,14 +1,17 @@
 const API="https://script.google.com/macros/s/AKfycby3wzak-aaQ66x5UKbC2_htO6H-qt9dp0eEsyUblO2_5X5t5b1Nd0FtZY4HCiaV6QBf_g/exec";
 
-function loadOrders(){
+function addProduct(){
 
-fetch(API+"?action=orders")
+let name=document.getElementById("name").value;
+
+let price=document.getElementById("price").value;
+
+let image=document.getElementById("image").value;
+
+fetch(API+"?action=addProduct&name="+name+"&price="+price+"&image="+image)
+
 .then(r=>r.json())
-.then(data=>{
 
-document.getElementById("orders").innerText=
-JSON.stringify(data,null,2);
-
-});
+.then(d=>alert("Product Added"));
 
 }
