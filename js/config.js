2b85ef14edaa0ea3efd866d23c6c46f73d4e102f
styleoflife987-hub <styleoflife987@gmail.com>
2026-03-15 21:07:34 +0530

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+    API_URL: "https://script.google.com/macros/s/AKfycby3wzak-aaQ66x5UKbC2_htO6H-qt9dp0eEsyUblO2_5X5t5b1Nd0FtZY4HCiaV6QBf_g/exec", // Replace after deployment
+    RAZORPAY_KEY: "rzp_test_SRYMp6P150uD4M" // Your test/live key
+};
