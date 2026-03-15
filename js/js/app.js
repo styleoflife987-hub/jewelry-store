061ec3e13ba7272083c33089d6d50a3ff0c03723
styleoflife987-hub <styleoflife987@gmail.com>
2026-03-15 14:30:39 +0530

    Update app.js

--- js/js/app.js
+++ js/js/app.js
@@ -1,9 +1,62 @@
-function addReview(product){
+const API="https://script.google.com/macros/s/AKfycby3wzak-aaQ66x5UKbC2_htO6H-qt9dp0eEsyUblO2_5X5t5b1Nd0FtZY4HCiaV6QBf_g/exec";
 
-let review=prompt("Write your review");
+let products=[];
 
-fetch(API+"?action=review&product="+product+"&text="+review);
+fetch(API+"?action=products")
+.then(r=>r.json())
+.then(data=>{
+products=data;
+showProducts(products);
+});
 
-alert("Review submitted");
+function showProducts(list){
+
+let html="";
+
+list.forEach(p=>{
+
+html+=`
+
+<div class="card">
+
+<img src="${p.image}">
+
+<h3>${p.name}</h3>
+
+<div class="price">₹${p.price}</div>
+
+<button onclick="addCart('${p.name}',${p.price})">
+Add To Cart
+</button>
+
+</div>
+
+`;
+
+});
+
+document.getElementById("products").innerHTML=html;
+
+}
+
+function searchProduct(q){
+
+let filtered=products.filter(p=>
+p.name.toLowerCase().includes(q.toLowerCase())
+);
+
+showProducts(filtered);
+
+}
+
+function addCart(name,price){
+
+let cart=JSON.parse(localStorage.getItem("cart")||"[]");
+
+cart.push({name,price});
+
+localStorage.setItem("cart",JSON.stringify(cart));
+
+alert(name+" added to cart");
 
 }
