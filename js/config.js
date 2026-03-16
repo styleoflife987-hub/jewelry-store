ff189d3b2e702a98374346b703de394e34024211
styleoflife987-hub <styleoflife987@gmail.com>
2026-03-16 07:05:00 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 // js/config.js
 const CONFIG = {
-    // Your deployed Google Apps Script URL (update after deployment)
-    API_URL: "https://script.google.com/macros/s/AKfycby3wzak-aaQ66x5UKbC2_htO6H-qt9dp0eEsyUblO2_5X5t5b1Nd0FtZY4HCiaV6QBf_g/exec",
+    // Your deployed Google Apps Script URL - UPDATE THIS AFTER DEPLOYMENT
+    API_URL: "https://script.google.com/macros/s/YOUR_DEPLOYED_ID/exec",
     
     // Razorpay Key (get from dashboard.razorpay.com)
-    RAZORPAY_KEY: "rzp_test_SRYMp6P150uD4M", // Get from Razorpay
+    RAZORPAY_KEY: "rzp_test_xxxxxxxxxx",
     
     // Site settings
     SITE_NAME: "Style Of Life Jewelry",
