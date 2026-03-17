2488f0986fb10bdc1a6333af6c253945f74dd502
styleoflife987-hub <styleoflife987@gmail.com>
2026-03-17 20:22:32 +0530

    Update payment.js

--- js/payment.js
+++ js/payment.js
@@ -7,18 +7,14 @@ function payNow(amount, customerDetails = {}) {
     
     var options = {
         key: CONFIG.RAZORPAY_KEY,
-        amount: amount * 100, // Razorpay expects amount in paise
+        amount: amount * 100,
         currency: "INR",
         name: CONFIG.SITE_NAME,
         description: "Jewelry Purchase",
         handler: function(response) {
             alert(`Payment Successful! Payment ID: ${response.razorpay_payment_id}`);
-            
-            // Clear cart
             localStorage.removeItem("cart");
             localStorage.removeItem('cartVersion');
-            
-            // Redirect to tracking page
             window.location.href = `track.html?order=${customerDetails.orderId || response.razorpay_payment_id}`;
         },
         prefill: {
@@ -28,11 +24,6 @@ function payNow(amount, customerDetails = {}) {
         },
         theme: {
             color: "#d4af37"
-        },
-        modal: {
-            ondismiss: function() {
-                console.log("Payment modal closed");
-            }
         }
     };
     
