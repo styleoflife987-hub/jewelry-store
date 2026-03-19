5c9f0d2cc07d29e05d888dd79c862512d71c1195
styleoflife987-hub <styleoflife987@gmail.com>
2026-03-19 21:55:24 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,5 @@
 // js/config.js
 const CONFIG = {
-    API_URL: "https://script.google.com/macros/s/AKfycbwCSWHfY18rKz_lNrZ7030S8OHJ-cjhEcTgW8MDYefpvqYJUzzl5rusDALMSOgkBmME/exec",
-    RAZORPAY_KEY: "rzp_test_SRYMp6P150uD4M",
     SITE_NAME: "Style Of Life Jewelry",
     CURRENCY: "₹",
     PLACEHOLDER_IMAGE: "https://via.placeholder.com/300x300?text=Style+Of+Life",
