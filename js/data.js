1d1a403297a283da35cc2ba76657cad393048a92
styleoflife987-hub <styleoflife987@gmail.com>
2026-03-20 06:46:02 +0530

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,14 +1,5 @@
-// js/data.js - Initialize data if not exists
+// js/data.js - Initialize with empty URLs
 (function() {
-    // Sample product images
-    const sampleImages = [
-        'https://images.unsplash.com/photo-1515562141207-7a88fb7ce338',
-        'https://images.unsplash.com/photo-1605100804763-247f67b3557e',
-        'https://images.unsplash.com/photo-1535632066927-ab7c9ab60908',
-        'https://images.unsplash.com/photo-1611591437281-460bfbe1220a',
-        'https://images.unsplash.com/photo-1573408301185-9146fe634ad0'
-    ];
-    
     // Initialize products if not exists
     if (!localStorage.getItem('products')) {
         const sampleProducts = [
@@ -20,7 +11,7 @@
                 price: 25000,
                 stock: 10,
                 description: '22k Gold Necklace with traditional design',
-                image: sampleImages[0]
+                images: ['', '', '', '', ''] // Empty URLs - you fill them
             },
             {
                 id: 2,
@@ -30,7 +21,7 @@
                 price: 45000,
                 stock: 5,
                 description: 'Solitaire Diamond Ring in 18k Gold',
-                image: sampleImages[1]
+                images: ['', '', '', '', '']
             },
             {
                 id: 3,
@@ -40,27 +31,7 @@
                 price: 15000,
                 stock: 8,
                 description: 'Freshwater Pearl Earrings with Gold',
-                image: sampleImages[2]
-            },
-            {
-                id: 4,
-                sku: 'SKU004',
-                name: 'Silver Bracelet',
-                category: 'Bracelets',
-                price: 12000,
-                stock: 15,
-                description: 'Sterling Silver Bracelet with design',
-                image: sampleImages[3]
-            },
-            {
-                id: 5,
-                sku: 'SKU005',
-                name: 'Gold Bangles',
-                category: 'Bangles',
-                price: 35000,
-                stock: 7,
-                description: 'Set of 2 Traditional Gold Bangles',
-                image: sampleImages[4]
+                images: ['', '', '', '', '']
             }
         ];
         localStorage.setItem('products', JSON.stringify(sampleProducts));
