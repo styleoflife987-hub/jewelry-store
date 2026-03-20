ba1836039ca8f5df5c097385ff6c59ed52e041bd
styleoflife987-hub <styleoflife987@gmail.com>
2026-03-20 19:25:14 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,5 +2,8 @@
 const CONFIG = {
     SITE_NAME: "Style Of Life Jewelry",
     CURRENCY: "₹",
-    DEFAULT_CATEGORY: "Jewelry"
+    DEFAULT_CATEGORY: "Jewelry",
+    
+    // IMPORTANT: Replace with your Google Apps Script URL
+    API_URL: "https://script.google.com/macros/s/AKfycbwCSWHfY18rKz_lNrZ7030S8OHJ-cjhEcTgW8MDYefpvqYJUzzl5rusDALMSOgkBmME/exec"
 };
