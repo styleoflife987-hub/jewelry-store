24a571cf8b59d809f5a9d5ad44227b4dea13af37
styleoflife987-hub <styleoflife987@gmail.com>
2026-03-21 22:06:49 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,10 +2,12 @@
 const CONFIG = {
     SITE_NAME: "Style Of Life Jewelry",
     CURRENCY: "₹",
+    DEFAULT_CATEGORY: "Jewelry",
     
-    // Your Project URL (from browser)
+    // Razorpay Test Key (Replace with your live key for production)
+    RAZORPAY_KEY: rzp_live_STwZarJ6EyJUPg
+    
+    // Supabase Credentials
     SUPABASE_URL: "https://tbopyyocuvlsjvtdogcp.supabase.co",
-    
-    // Your Publishable Key (from the table above)
     SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRib3B5eW9jdXZsc2p2dGRvZ2NwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQwMzYxNzcsImV4cCI6MjA4OTYxMjE3N30.TfEtU1NBHCGnZqZzm4LuI1iw22lSPav0OrUskhDw5wc"
 };
