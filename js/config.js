d8a437b7b12ed9bc32969adff87231ef48b19109
styleoflife987-hub <styleoflife987@gmail.com>
2026-03-21 07:10:56 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -7,5 +7,5 @@ const CONFIG = {
     SUPABASE_URL: "https://tbopyyocuvlsjvtdogcp.supabase.co",
     
     // Your Publishable Key (from the table above)
-    SUPABASE_ANON_KEY: "sb_publishable_Z5eyvKGa72lR8Mv4uWbVTA_uTvqRSaS"
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRib3B5eW9jdXZsc2p2dGRvZ2NwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQwMzYxNzcsImV4cCI6MjA4OTYxMjE3N30.TfEtU1NBHCGnZqZzm4LuI1iw22lSPav0OrUskhDw5wc"
 };
